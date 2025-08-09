1102665dccbcb17d67220b09c718690fd63978fe
melwin <114172287+itzTedx@users.noreply.github.com>
2025-08-09 13:32:08 +0400

    Refactor biome.json to streamline file inclusion settings and enhance linter rules for better code quality. Update package.json and pnpm-lock.yaml to add new dependencies including @hookform/resolvers, @radix-ui/react-label, @radix-ui/react-slot, react-hook-form, and zod. Modify footer component to include a contact form and improve layout for better user engagement.

---
+++ src/components/contact-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { Textarea } from "./ui/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  message: z.string().min(10, {
+    message: "Message must be at least 10 characters.",
+  }),
+});
+
+export function ContactForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="sr-only">Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="sr-only">Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Message" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,20 +2,39 @@ import Link from "next/link";
 
 import { IconHeart } from "@/assets/icons/heart";
 
+import { ContactForm } from "../contact-form";
 import { Badge } from "../ui/badge";
 
 export function Footer() {
   return (
     <footer className="bg-sky-100">
-      <div className="container relative max-w-7xl border-x py-2">
-        <div className="mx-auto grid max-w-5xl grid-cols-3 gap-3">
+      <div className="container relative max-w-7xl border-x pb-20">
+        <div className="mx-auto grid max-w-5xl grid-cols-3 gap-14">
           <div>
             <Badge className="font-light text-base text-muted tracking-wide" variant="outline">
               Remembered with <IconHeart className="!size-6 shrink-0" />
             </Badge>
-            <p className="font-light font-sans text-3xl">
+            <h2 className="font-light font-sans text-3xl">
               Keep Reena’s spirit alive by <span className="font-medium">making a difference</span> in other’s lives.
-            </p>
+            </h2>
+          </div>
+          <div className="relative col-span-2 ml-12 bg-sky-200 p-2.5">
+            <div className="rounded-2xl bg-white">
+              <div className="border-b p-4 text-center">
+                <h3 className="font-sans text-xl">We welcome your stories, tributes, and messages of support.</h3>
+                <p className="text-balance text-sm">
+                  Whether you’d like to share a memory of Reena, learn more about our initiatives, or find out how to
+                  contribute, please reach out.
+                </p>
+              </div>
+              <div className="p-8">
+                <ContactForm />
+              </div>
+              <div className="absolute bottom-1.5 left-1.5 z-1 size-1.5 rounded-full bg-white" />
+              <div className="absolute right-1.5 bottom-1.5 z-1 size-1.5 rounded-full bg-white" />
+              <div className="absolute top-1.5 left-1.5 z-1 size-1.5 rounded-full bg-white" />
+              <div className="absolute top-1.5 right-1.5 z-1 size-1.5 rounded-full bg-white" />
+            </div>
           </div>
         </div>
         <span aria-hidden="true" className="-bottom-1 -left-1 absolute z-1 size-1.5 rounded-full bg-white" />
