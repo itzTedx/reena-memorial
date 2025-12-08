37660dadd1adbef5f7f1db9e0de4ed3858ffd7c2
melwin <114172287+itzTedx@users.noreply.github.com>
2025-12-08 12:31:41 +0400

    Upgrade Next.js to version 16.0.7 and remove standalone output configuration. Update package.json and pnpm-lock.yaml for dependency management, including adjustments to sharp and other related packages for improved compatibility and performance.

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,8 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 
   logging: {
